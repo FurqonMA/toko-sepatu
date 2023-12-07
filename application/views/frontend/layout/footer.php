491a91fb0c14335641cb56c855bc7afd9566b834
FurqonMA <surengpati5@gmail.com>
2023-12-07 14:59:12 +0700

    membuat testimonials

--- application/views/frontend/layout/footer.php
+++ application/views/frontend/layout/footer.php
@@ -1,9 +1,64 @@
 <section>
+  <a href="https://wa.me/6282324107028" target="_blank" class="btn-whatsapp">
+    <div class="section">
+    <i class="fa-brands fa-whatsapp"><span class="tooltip">WhatsApp</span></i>
+    </div>
+  </a>
+  <div class="testimonials">
+    <div class="inner">
+      <h2>Testimoials From Superstars</h2>
+      <div class="border"></div>
+      <div class="row row-testi">
+        <div class="col-lg-4 col-testi">
+          <div class="testimonial">
+             <img src="<?=base_url();?>/assets/img/dominik.jpeg" alt="">
+             <div class="name">Dominik Szoboszlai</div>
+             <div class="stars">
+              <i class='bx bxs-star'></i>
+              <i class='bx bxs-star'></i>
+              <i class='bx bxs-star'></i>
+              <i class='bx bxs-star'></i>
+              <i class='bx bxs-star'></i>
+             </div>
+             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates fugit, expedita voluptatum minima soluta minus aliquid. Voluptatibus, magni ducimus totam nam unde enim iure alias iusto ipsum omnis hic eaque.</p>
+          </div>
+        </div>
+        <div class="col-lg-4 col-testi">
+          <div class="testimonial">
+             <img src="<?=base_url();?>/assets/img/jordan.jpg" alt="">
+             <div class="name">Michael Jordan</div>
+             <div class="stars">
+              <i class='bx bxs-star'></i>
+              <i class='bx bxs-star'></i>
+              <i class='bx bxs-star'></i>
+              <i class='bx bxs-star'></i>
+              <i class='bx bxs-star'></i>
+             </div>
+             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates fugit, expedita voluptatum minima soluta minus aliquid. Voluptatibus, magni ducimus totam nam unde enim iure alias iusto ipsum omnis hic eaque.</p>
+          </div>
+        </div>
+        <div class="col-lg-4 col-testi">
+          <div class="testimonial">
+             <img src="<?=base_url();?>/assets/img/ricardinho.jpg" alt="">
+             <div class="name">Ricardinho</div>
+             <div class="stars">
+              <i class='bx bxs-star'></i>
+              <i class='bx bxs-star'></i>
+              <i class='bx bxs-star'></i>
+              <i class='bx bxs-star'></i>
+              <i class='bx bxs-star'></i>
+             </div>
+             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates fugit, expedita voluptatum minima soluta minus aliquid. Voluptatibus, magni ducimus totam nam unde enim iure alias iusto ipsum omnis hic eaque.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
   <div class="container-fluid mb-0" style="background-color: rgb(253, 180, 22); margin-bottom: -9px; height: auto;">
       <div class="kotak">
         <img src="<?=base_url();?>/assets/img/kelebihan.png" alt="">
       </div>
-    </div>
+  </div>
   <footer class="footer">
     <div class="container">
       <div class="row">
@@ -21,7 +76,7 @@
         </div>
         <div class="col-lg-4 col-md-6 col-sm-12 mt-5 contact">
           <h4 class="title-footer">Customer Service</h4>
-          <a href="#"><i class='bx bxs-phone'></i> <h5> :&nbsp;&nbsp;08123456789</h5></a>
+          <a href="https://wa.me/6282324107028" target="_blank"><i class='bx bxs-phone'></i> <h5> :&nbsp;&nbsp;+62 823-2410-7028</h5></a>
           <a href="#"><i class='bx bxl-telegram'></i> <h5> :&nbsp;&nbsp;AthleticXpress</h5></a>
           <a href="https://www.google.com/maps/place/FOTO+COPY+%26+ALAT+TULIS+OMEGA/@-6.4968002,110.9026854,3a,75y,178.44h,93.34t/data=!3m7!1e1!3m5!1snWVLr2pSTSkaIzA2NJ4Cog!2e0!6shttps:%2F%2Fstreetviewpixels-pa.googleapis.com%2Fv1%2Fthumbnail%3Fpanoid%3DnWVLr2pSTSkaIzA2NJ4Cog%26cb_client%3Dsearch.revgeo_and_fetch.gps%26w%3D96%26h%3D64%26yaw%3D215.45996%26pitch%3D0%26thumbfov%3D100!7i16384!8i8192!4m16!1m9!3m8!1s0x2e7125624170552b:0x5027a76e3561600!2sKelet,+Kec.+Keling,+Kabupaten+Jepara,+Jawa+Tengah!3b1!8m2!3d-6.4969203!4d110.9059574!10e5!16s%2Fg%2F120vt34z!3m5!1s0x2e7125627e8f2321:0x9cb5bab6349d29f1!8m2!3d-6.4968809!4d110.9026601!16s%2Fg%2F11v5yxk0f_?entry=ttu" target="_blank"><i class='bx bxs-map'></i> <h5> :&nbsp; JL. Raya Kelet, Keling, Jepara</h5></a>
         </div>
--- application/views/frontend/layout/head.php
+++ application/views/frontend/layout/head.php
@@ -15,6 +15,9 @@
     <!-- BOX ICON CDN CSS -->
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 
+    <!-- FONT AWESOME -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
     <!-- JQUERY -->
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
 
